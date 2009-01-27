96f65abc75f76b158bba9a66f5623865a757162a
Kengo NAGASHIMA <nagasima@josai.ac.jp>
2009-01-27 03:02:08 +0000

    version 1.0-0

---
+++ R/print.mmcm.resamp.R
@@ -0,0 +1,28 @@
+`print.mmcm.resamp` <-
+function(x, ...) {
+  ####################
+  # print result
+  ####################
+  if (length(unique(x$astat))!=length(x$astat)) {
+    cat(sprintf("\n\tModified maximum contrast method\n\n"))
+    cat("more than 2 contrast vectors was selected ...\n\n")
+    cat(
+      sprintf("selected contrast = "),
+      sprintf("%d ", x$cont),
+      sprintf("\nP-values = "),
+      sprintf("%f ", x$pval),
+      sprintf("\b\n\n"), sep=""
+    )
+  } else {
+    cat(
+      sprintf("\n\tModified maximum contrast method\n\n"),
+      sprintf("observed statistics = %.3f\n", x$astat[x$cont]),
+      sprintf("contrast = %d\t(", x$cont),
+      sprintf("%s ", x$acont[,x$cont]),
+      sprintf("\b)\n"),
+      sprintf("P-value = %f\n\n", x$pval), sep=""
+    )
+  }
+  invisible(0)
+}
+
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+`.First.lib` <-
+function(lib, pkg) {
+  library.dynam("mmcm", pkg, lib)
+}
+
